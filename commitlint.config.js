147346480c405ae253eee1c03d1c8339e30803b4
GRIT <>
2021-06-23 10:41:52 +0800

    feat: 代码工作流配置

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Description: 输入文件描述信息
+ * @Author: liu-wb
+ * @Date: 2021-06-23 10:37:19
+ * @LastEditors: liu-wb
+ * @LastEditTime: 2021-06-23 10:37:19
+ * @FilePath: /node-js/commitlint.config.js
+ */
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+};
